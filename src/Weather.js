dcfe0ad86cbdd0b0304dffdcf700a51280784a9b
Kearabetswejabane <keahjabane@gmail.com>
2024-06-16 20:18:34 +0200

    Changed the h2 Content

--- src/Weather.js
+++ src/Weather.js
@@ -8,5 +8,5 @@ export default function Weather(props) {
   let apiKey = "597c40c39084687093b091cd48b366f8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?g=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return <h2>Hello from React</h2>;
 }
